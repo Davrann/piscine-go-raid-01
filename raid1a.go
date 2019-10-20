45038a22258bd18d2069caca7b15f333f40bd7c2
snooprogg <adil.110194@gmail.com>
2019-10-20 15:56:43 +0600

    raid1a: first commit

---
+++ raid1a.go
@@ -0,0 +1,43 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func Raid1a(x, y int) {
+	var tl rune = 'o'
+	var tr rune = 'o'
+	var bl rune = 'o'
+	var br rune = 'o'
+	var w rune = '-'
+	var h rune = '|'
+	var s rune = ' '
+	var eol rune = '\n'
+
+	z01.PrintRune(tl)
+	for i := 0; i < x-2; i++ {
+		z01.PrintRune(w)
+	}
+	if x > 1 {
+		z01.PrintRune(tr)
+	}
+	z01.PrintRune(eol)
+	for i := 0; i < y-2; i++ {
+		z01.PrintRune(h)
+		for j := 0; j < x-2; j++ {
+			z01.PrintRune(s)
+		}
+		if x > 1 {
+			z01.PrintRune(h)
+		}
+		z01.PrintRune(eol)
+	}
+	if y > 1 {
+		z01.PrintRune(bl)
+		for i := 0; i < x-2; i++ {
+			z01.PrintRune(w)
+		}
+		if x > 1 {
+			z01.PrintRune(br)
+		}
+		z01.PrintRune(eol)
+	}
+}
