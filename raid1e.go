49f426f9e70089b87244304ab20a41892bfd84bd
adil.shibikhanov <adil.shibikhanov@dits.kz>
2019-10-21 16:29:00 +0600

    if added

--- raid1e.go
+++ raid1e.go
@@ -12,32 +12,34 @@ func Raid1e(x, y int) {
 	var s rune = ' '
 	var eol rune = '\n'
 
-	z01.PrintRune(tl)
-	for i := 0; i < x-2; i++ {
-		z01.PrintRune(w)
-	}
-	if x > 1 {
-		z01.PrintRune(tr)
-	}
-	z01.PrintRune(eol)
-	for i := 0; i < y-2; i++ {
-		z01.PrintRune(h)
-		for j := 0; j < x-2; j++ {
-			z01.PrintRune(s)
-		}
-		if x > 1 {
-			z01.PrintRune(h)
-		}
-		z01.PrintRune(eol)
-	}
-	if y > 1 {
-		z01.PrintRune(bl)
+	if x > 0 && y > 0 {
+		z01.PrintRune(tl)
 		for i := 0; i < x-2; i++ {
 			z01.PrintRune(w)
 		}
 		if x > 1 {
-			z01.PrintRune(br)
+			z01.PrintRune(tr)
 		}
 		z01.PrintRune(eol)
+		for i := 0; i < y-2; i++ {
+			z01.PrintRune(h)
+			for j := 0; j < x-2; j++ {
+				z01.PrintRune(s)
+			}
+			if x > 1 {
+				z01.PrintRune(h)
+			}
+			z01.PrintRune(eol)
+		}
+		if y > 1 {
+			z01.PrintRune(bl)
+			for i := 0; i < x-2; i++ {
+				z01.PrintRune(w)
+			}
+			if x > 1 {
+				z01.PrintRune(br)
+			}
+			z01.PrintRune(eol)
+		}
 	}
 }
